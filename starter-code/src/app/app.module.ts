6076b8b5e829e99ac01bf11cb76b732b4c5a5d6c
Jana Stinn <jana-stinn@gmx.de>
2018-02-21 20:05:19 +0100

    started second iteration

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { MovieComponentComponent } from './components/movie-component/movie-component.component';
+
 import { MoviesServiceService } from 'app/services/movies-service.service';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: HomeComponentComponent },
+  { path: 'about', component: MovieComponentComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    MovieComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [MoviesServiceService],
   bootstrap: [AppComponent]
---
+++ starter-code/src/app/components/movie-component/movie-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-component',
+  templateUrl: './movie-component.component.html',
+  styleUrls: ['./movie-component.component.css']
+})
+export class MovieComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
